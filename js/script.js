6647265e6ca87cc9641cf3717da999a1487d0f87
jua850623 <142187018+jua850623@users.noreply.github.com>
2023-12-15 11:32:00 +0900

    maesil

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(".introduce").click(
+    function(){
+        $("#contents").show();
+    }
+);
+$(".close").click(
+    function(){
+        $("#contents").hide();
+    }
+);
+$(".flower").hover(
+    function(){
+        $(".close").show();
+    }
+);
+$(".imgimg").click(
+    function(){
+        $("#contents4").show();
+        
+    }
+);
+$(".clcl").click(
+    function(){
+        $("#contents4").hide();
+        
+    }
+);
+// $(".sol").click(
+//     function(){
+//         $("#charge").show();
+//     }
+// );
+// $(".bang").click(
+//     function(){
+//         $("#information").show();
+//     }
+// );
+// $(".ul").click(
+//     function(){
+//         $("#option").show();
+//     }
+// );
+// $(".solbangul").click(
+//     function(){
+//         $("#policy").show();
+//     }
+// );
+
+
